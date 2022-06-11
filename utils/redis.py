93135e6430a32632648295a8260062a57f65f9b1
Shylajhaa <rsshylu@hotmail.com>
2022-06-11 21:26:06 +0530

    Redis changes

---
+++ utils/redis.py
@@ -0,0 +1,15 @@
+import redis
+
+class RedisUtil():
+    def __init__(self) -> None:
+        redis_client = redis.Redis()
+        pass
+
+    def set(self, key, value):
+        pass
+
+    def set_all(self, key, value):
+        pass
+
+    def get(self, key):
+        pass
